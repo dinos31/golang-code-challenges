94a8328758c770b454a666767a6255aded516ac5
mateo <m.sierra1037@hotmail.com>
2021-04-04 13:36:37 -0500

    second day of learning go

--- clock.go
+++ clock.go
@@ -4,6 +4,21 @@ import (
 	"strconv"
 )
 
+func Solution(word string) string {
+	// this is how you access string positions
+	// this was an 8kyu kata in codewars.com
+	var (
+		answer  string
+		counter int = len(word) - 1
+	)
+	for i := 0; i < len(word); i++ {
+
+		answer = answer + string(word[counter])
+		counter = counter - 1
+	}
+
+	return answer
+}
 func countSeconds(userInput int) string {
 	// use int(f+number.decimal) to round up
 	if userInput > 359999 {
@@ -17,6 +32,16 @@ func countSeconds(userInput int) string {
 		minutes string = strconv.Itoa(int(int(userInput%3600) / 60))
 		seconds string = strconv.Itoa(int(int(userInput%3600) % 60))
 	)
+
+	if len(hours) == 1 {
+		hours = "0" + hours
+	}
+	if len(minutes) == 1 {
+		minutes = "0" + minutes
+	}
+	if len(seconds) == 1 {
+		seconds = "0" + seconds
+	}
 
 	return hours + ":" + minutes + ":" + seconds
 }
--- hello.go
+++ hello.go
@@ -2,45 +2,21 @@ package main
 
 import (
 	"fmt"
-
-	"rsc.io/quote"
 )
 
-//initialized variable with type declaration
-var goint int
-var gostring string = "this string is strongly typed"
-var gofloat float32 = 23
-
-//there are two ways of declaring multiple variables at the time
-var (
-	myString string = "string inside block declarations"
-	myInt    int    = 123
-) //var block
-
-// this is the equivalent of a while loop
-// for goint < 10 {
-// 	fmt.Println(goint + 1)
-// 	goint++
-// }
-// classic for loop in go
-//for i := 0; i < 10; i++{
-//   your code here
-//}
+//remember to add formated strings with %v and %t
+// remember to add for/ while loops and block declaration
+//and := this weird ass syntax
+// also add the scanln for input and the
+// string position accessor
 
 func main() {
-	// this syntax detects variable type, but it has
-	// to be written and USED inside a function
-	//weirdSyntax := `some weird syntax`
-
-	// the following is a formated print
-	// formated text keyLetters v = value, T = variable's type
-	// you can also use a %T to print the variable's type
-	fmt.Printf("\n%v\n",
-		quote.Opt())
-
-	fmt.Println("enter # of seconds")
-	var seconds int
-	fmt.Scanln(&seconds)
-
-	fmt.Printf("clock : %v", countSeconds(seconds))
+	//fmt.Println("enter # of seconds")
+	// var seconds int
+	// fmt.Scanln(&seconds)
+	// fmt.Printf("clock : %v\n", countSeconds(seconds))
+	fmt.Println("enter a word")
+	var myWord string
+	fmt.Scanln(&myWord)
+	fmt.Println(Solution(myWord))
 }
