fb2fdf8263010eb728acc604afbb6a411da64910
mateo <m.sierra1037@hotmail.com>
2021-04-06 13:34:27 -0500

    reorganized files

--- codeChalls_1.go
+++ codeChalls_1.go
@@ -1,11 +1,32 @@
 package main
 
+import (
+	"strconv"
+)
+
 // from codeWars (8kyu)
 func EvenOrOdd(number int) string {
 	if number%2 == 0 {
 		return "Even"
 	}
 	return "Odd"
+}
+
+// invert string, from codeWars (8kyu)
+func Solution(word string) string {
+	// this is how you access string positions
+	// this was an 8kyu kata in codewars.com
+	var (
+		answer  string
+		counter int = len(word) - 1
+	)
+	for i := 0; i < len(word); i++ {
+
+		answer = answer + string(word[counter])
+		counter = counter - 1
+	}
+
+	return answer
 }
 
 // from codewars (8kyu)
@@ -40,4 +61,32 @@ func Arithmetic(a, b int, operator string) int {
 	default:
 		panic("Unknown operator")
 	}
+}
+
+// seconds to readable time, from codeWars (6kyu)
+func countSeconds(userInput int) string {
+	// use int(f+number.decimal) to round up
+	if userInput > 359999 {
+		userInput = 359999
+	}
+	if userInput < 0 {
+		userInput = -(userInput)
+	}
+	var (
+		hours   string = strconv.Itoa(int(userInput / 3600))
+		minutes string = strconv.Itoa(int(int(userInput%3600) / 60))
+		seconds string = strconv.Itoa(int(int(userInput%3600) % 60))
+	)
+
+	if len(hours) == 1 {
+		hours = "0" + hours
+	}
+	if len(minutes) == 1 {
+		minutes = "0" + minutes
+	}
+	if len(seconds) == 1 {
+		seconds = "0" + seconds
+	}
+
+	return hours + ":" + minutes + ":" + seconds
 }
