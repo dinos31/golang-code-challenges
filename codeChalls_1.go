22d4ed658120acc06e0f2d6f7ccaa12331c4d0ff
mateo <m.sierra1037@hotmail.com>
2021-04-06 19:06:36 -0500

    more exercises

--- codeChalls_1.go
+++ codeChalls_1.go
@@ -12,6 +12,26 @@ func EvenOrOdd(number int) string {
 	return "Odd"
 }
 
+// from codewars 8kyu
+func Summation(n int) int {
+	var acum int
+	for i := 0; i <= n; i++ {
+		acum += i
+	}
+	return acum
+}
+
+// from codewars 8kyu
+func Move(position int, roll int) int {
+	// your code here
+	return position + roll*2
+}
+
+// from codewars 8kyu
+func RemoveChar(word string) string {
+	return word[1 : len(word)-1]
+}
+
 // invert string, from codeWars (8kyu)
 func Solution(word string) string {
 	// this is how you access string positions
@@ -33,6 +53,19 @@ func Solution(word string) string {
 func OtherAngle(a int, b int) int {
 
 	return 180 - (a + b)
+}
+
+//from codewars(7kyu)
+func TwoToOne(s1 string, s2 string) string {
+	var (
+		noDuplicates string = ""
+	)
+	for i := 0; i < len(s1); i++ {
+		if s1[i] == noDuplicates[i] {
+			noDuplicates += string(s1[i])
+		}
+	}
+	return s1 + s2
 }
 
 //from codewars (7kyu)
--- learnGo.go
+++ learnGo.go
@@ -7,29 +7,11 @@ import "fmt"
 //var c, python, java = true, false, "no!"
 
 func main() {
+	// var a []int
+	// for i := 0; i < 10; i++ {
+	// 	a = append(a, i+1)
+	// 	fmt.Printf("%v\n", a[i])
 
-	var cantidadValores int
-
-	fmt.Println("cuantos valores desea ingresar?")
-	fmt.Scanln(&cantidadValores)
-
-	var negativos, ceros, positivos, input int
-
-	for i := 0; i < int(cantidadValores); i++ {
-		fmt.Printf("\ningrese valor # %v :", i+1)
-		fmt.Scanln(&input)
-		if input > 0 {
-			positivos++
-		}
-		if input == 0 {
-			ceros++
-		}
-		if input < 0 {
-			negativos++
-		}
-	}
-
-	fmt.Printf(
-		"la cantidad de #'s positivos es de : %v \n\n la cantidad de #'s Negativos es de : %v \n \n la cantidad de ceros es de : %v \n ",
-		positivos, negativos, ceros)
+	// }
+	fmt.Println(RemoveChar("word"))
 }
